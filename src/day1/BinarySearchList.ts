2c4ab94e5e33557ed50e7be91e935727b50761cf
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-08 11:26:15 +0100

    more algorithms

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+	let lowIndex = 0;
+	let highIndex = haystack.length;
 
+	do {
+		const midIndex = Math.floor((lowIndex + highIndex) / 2);
+
+		if (haystack[midIndex] === needle) {
+			return true;
+		}
+		if (haystack[midIndex] > needle) {
+			highIndex = midIndex;
+		}
+		else {
+			lowIndex = midIndex + 1;
+		}
+
+	} while (lowIndex < highIndex);
+
+	return false;
 }
--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,17 @@
 export default function bubble_sort(arr: number[]): void {
+	// Outer loop controls the number of passes through the array
+	for (let i = 0; i < arr.length; i++) {
+		// Inner loop iterates through the unsorted portion of the array
+		for (let j = 0; j < arr.length - 1 - i; j++) {
+			// Compare adjacent elements and swap if they are in the wrong order
 
+			// Store the value at arr[j] in a temporary variable
+			const temp = arr[j];
+			if (arr[j] > arr[j + 1]) {
+				// Swap the elements if they are out of order
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
 }
